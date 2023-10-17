ca81d1d9fe10b70cf88b15f3dc34931e4583d188
eyuuab <eyobedabrham9@gmail.com>
2023-10-17 16:59:33 +0300

    importing defaltdict

--- genPassword.py
+++ genPassword.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+from collections import defaultdit
 def generate_random_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
